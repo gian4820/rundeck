a8f87f2c98e566542447edee8994ff57612e1db2
Ginfranco Groppo <48934140+gian4820@users.noreply.github.com>
2023-11-13 16:16:48 -0300

    Update hello_github.sh

--- hello_github.sh
+++ hello_github.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 echo "Hello from github this is a new change"
-echo @$RD_OPTION_CARS
-echo ${unquotedoption.cars}
-echo ${option.cars}
-echo @option.cars@
-
-echo "@$RD_OPTION_CARS"
-echo "${unquotedoption.cars}"
-echo "${option.cars}"
-echo "@option.cars@"
